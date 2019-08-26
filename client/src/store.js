808981e759106ab93ad3c8696ab8878095cc121e
jpimentel45 <jpimentel45@gmail.com>
2019-08-26 10:42:36 -0700

    changing store

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,19 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const intitialState ={};
+const initialState ={};
 const middleware = [thunk];
-const store = createStore(rootReducer, intitialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+if (process.env.NODE_ENV === 'production') {
+    store = createStore(rootReducer, initialState, compose(
+        applyMiddleware(...middleware)
+    ));
+} else {
+    store = createStore(rootReducer, initialState, compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ));
+}
+
 
 // const store = createStore(rootReducer, intitialState, compose(
 //     applyMiddleware(...middleware),
